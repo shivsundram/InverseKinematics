4585a89ff11cd4c181f745eebff8e57a96685ccf
Shiv Sundram <shivsundram@berkeley.edu>
2014-12-08 19:53:27 -0800

    arm

--- InverseKinematics.cpp
+++ InverseKinematics.cpp
@@ -1,3 +1,7 @@
+// InverseKinematics.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
 #include <vector>
 #include <iostream>
 #include <cmath>
@@ -36,186 +40,277 @@ using namespace std;
 using namespace Eigen;
 
 
-const double THETA_CHANGE = 0.001;
+const double THETA_CHANGE = 0.1;
 
 
 Vector3d rotateX(const Vector3d& p, double theta) {
-    Matrix3d rotateX;
-    rotateX <<
-        1, 0, 0,
-        0, cos(theta), -sin(theta),
-        0, sin(theta), sin(theta);
+	Matrix3d rotateX;
+	rotateX <<
+		1, 0, 0,
+		0, cos(theta), -sin(theta),
+		0, sin(theta), cos(theta);
 
-    return rotateX * p;
+	return rotateX * p;
 }
 
 Vector3d rotateY(const Vector3d& p, double theta) {
-    Matrix3d rotateY;
-    rotateY <<
-        cos(theta), 0, sin(theta),
-        0, 1, 0,
-        -sin(theta), 0, cos(theta);
+	Matrix3d rotateY;
+	rotateY <<
+		cos(theta), 0, sin(theta),
+		0, 1, 0,
+		-sin(theta), 0, cos(theta);
 
-    return rotateY * p;
+	return rotateY * p;
 }
 
 Vector3d rotateZ(const Vector3d& p, double theta) {
-    Matrix3d rotateZ;
-    rotateZ <<
-        cos(theta), -sin(theta), 0,
-        sin(theta), sin(theta), 0,
-        0, 0, 1;
+	Matrix3d rotateZ;
+	rotateZ <<
+		cos(theta), -sin(theta), 0,
+		sin(theta), cos(theta), 0,
+		0, 0, 1;
 
-    return rotateZ * p;
+	return rotateZ * p;
 }
 
 
 Vector3d translate(const Vector3d& p, const Vector3d& to) {
-    return p - to;
+	return p - to;
 }
 
 
 
 class Viewport {
 public:
-    int w, h; // width and height
+	int w, h; // width and height
 };
 
 
 class Joint {
 public:
-    double length;
-    Vector3d theta;
-    Vector3d basePoint, endPoint;
+	double length;
+	Vector3d theta;
+	Vector3d basePoint, endPoint, first, second, third, fourth;
 
-    void update(Vector3d dtheta, Vector3d newBasePoint);
-    void render();
+	void update(Vector3d dtheta, Vector3d newBasePoint);
+	void render();
 
-    Joint(Vector3d inBasePoint, double inLength);
+	Joint(Vector3d inBasePoint, double inLength);
 };
 
 
 Joint::Joint(Vector3d inBasePoint, double inLength) {
-    length = inLength;
-    theta = Vector3d(0.0, 0.0, 0.0);
-    basePoint = inBasePoint;
-    endPoint = basePoint + Vector3d(0.0, length, 0.0);
+	length = inLength;
+	theta = Vector3d(0.0, 0.0, 0.0);
+	basePoint = inBasePoint;
+	endPoint = basePoint + Vector3d(0.0, length, 0.0);
+
+	double d = length / 5;
+	first    = basePoint + Vector3d(-d, length/3, -d);
+	second   = basePoint + Vector3d(d, length/3, -d);
+	third    = basePoint + Vector3d(d, length/3, d);
+	fourth   = basePoint + Vector3d(-d, length/3, d);
 }
 
 void Joint::update(Vector3d dtheta, Vector3d newBasePoint) {
-    basePoint = newBasePoint;
+	basePoint = newBasePoint;
 
-    Vector3d translated = translate(endPoint, newBasePoint);
+	Vector3d original(0.0, length, 0.0);
+	
+	double d = length / 5; 
+	Vector3d ofirst = Vector3d(-d, length/3, -d);
+	Vector3d osecond =Vector3d(d, length/3, -d);
+	Vector3d othird = Vector3d(d, length/3, d);
+	Vector3d ofourth = Vector3d(-d, length/3, d);
 
-    Vector3d newEndPoint = rotateZ(rotateY(rotateX(translated, dtheta[0]), dtheta[1]), dtheta[2]);
-    endPoint = translate(newEndPoint, -newBasePoint);
+
+	theta += dtheta;
+
+	Vector3d newEndPoint = rotateZ(rotateY(rotateX(original, theta[0]), theta[1]), theta[2]);
+
+	Vector3d newFirst = rotateZ(rotateY(rotateX(ofirst, theta[0]), theta[1]), theta[2]);
+	Vector3d newSecond = rotateZ(rotateY(rotateX(osecond, theta[0]), theta[1]), theta[2]);
+	Vector3d newThird = rotateZ(rotateY(rotateX(othird, theta[0]), theta[1]), theta[2]);
+	Vector3d newFourth = rotateZ(rotateY(rotateX(ofourth, theta[0]), theta[1]), theta[2]);
+
+	endPoint = translate(newEndPoint, -newBasePoint);
+
+	first = translate(newFirst, -newBasePoint);
+	second = translate(newSecond, -newBasePoint);
+	third = translate(newThird, -newBasePoint);
+	fourth = translate(newFourth, -newBasePoint);
+
 }
 
 void Joint::render() {
-    glBegin(GL_LINES);
-        glVertex3d(basePoint[0], basePoint[1], basePoint[2]);
-        glVertex3d(endPoint[0], endPoint[1], endPoint[2]);
-    glEnd();
+	//first half
+	glBegin(GL_POLYGON);
+	glVertex3d(basePoint[0], basePoint[1], basePoint[2]);
+	glVertex3d(first[0], first[1], first[2]);
+	glVertex3d(second[0], second[1], second[2]);
+	glEnd();
+	
+	glBegin(GL_POLYGON);
+	glVertex3d(basePoint[0], basePoint[1], basePoint[2]);
+	glVertex3d(second[0], second[1], second[2]);
+	glVertex3d(third[0], third[1], third[2]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(basePoint[0], basePoint[1], basePoint[2]);
+	glVertex3d(third[0], third[1], third[2]);
+	glVertex3d(fourth[0], fourth[1], fourth[2]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(basePoint[0], basePoint[1], basePoint[2]);
+	glVertex3d(fourth[0], fourth[1], fourth[2]);
+	glVertex3d(first[0], first[1], first[2]);
+	glEnd();
+
+	//second 
+	glBegin(GL_POLYGON);
+	glVertex3d(endPoint[0], endPoint[1], endPoint[2]);
+	glVertex3d(first[0], first[1], first[2]);
+	glVertex3d(second[0], second[1], second[2]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(endPoint[0], endPoint[1], endPoint[2]);
+	glVertex3d(second[0], second[1], second[2]);
+	glVertex3d(third[0], third[1], third[2]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(endPoint[0], endPoint[1], endPoint[2]);
+	glVertex3d(third[0], third[1], third[2]);
+	glVertex3d(fourth[0], fourth[1], fourth[2]);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glVertex3d(endPoint[0], endPoint[1], endPoint[2]);
+	glVertex3d(fourth[0], fourth[1], fourth[2]);
+	glVertex3d(first[0], first[1], first[2]);
+	glEnd();
+
 }
 
 
 
 class System {
 private:
-    vector<Joint> joints;
-    MatrixXd getJacobian();
-    void updateJoints(const VectorXd& dtheta);
+	vector<Joint> joints;
+	MatrixXd getJacobian();
+	void updateJoints(const VectorXd& dtheta);
 public:
 
-    System(vector<Joint> inJoints);
-    bool update(Vector3d g);
-    void render();
+	System(vector<Joint> inJoints);
+	bool update(Vector3d g);
+	void render();
 
 };
 
 
 System::System(vector<Joint> inJoints) {
-    joints = inJoints;
+	joints = inJoints;
 }
 
 
 MatrixXd System::getJacobian() {
-    MatrixXd result(3, 3 * joints.size());
+	MatrixXd result(3, 3 * joints.size());
 
-    Vector3d deltaTheta(1, 1, 1);
-    deltaTheta *= THETA_CHANGE;
+	Vector3d deltaTheta(1.0, 1.0, 1.0);
+	deltaTheta *= THETA_CHANGE;
 
-    for (int i = 0; i < joints.size(); i++) {
-        Vector3d newTheta = joints[i].theta + deltaTheta;
+	for (int i = 0; i < joints.size(); i++) {
+		Vector3d newTheta = joints[i].theta + deltaTheta;
 
-        Vector3d original = translate(joints[i].endPoint, joints[i].basePoint);
+		Vector3d original(0.0, joints[i].length, 0.0); //translate(joints[i].endPoint, joints[i].basePoint);
 
-        Vector3d p = rotateX(original, joints[i].theta[0]);
-        Vector3d newP = rotateX(original, newTheta[0]);
 
-        Vector3d col1 = (p - newP) / THETA_CHANGE;
+		Vector3d p = rotateX(original, joints[i].theta[0]);
+		Vector3d newP = rotateX(original, newTheta[0]);
 
-        result(i * 3, 0) = col1[0];
-        result(i * 3, 1) = col1[1];
-        result(i * 3, 2) = col1[2];
+		Vector3d col1 = (newP - p) / THETA_CHANGE;
 
-        p = rotateY(original, joints[i].theta[1]);
-        newP = rotateY(original, newTheta[1]);
+		result(0, i * 3) = col1[0];
+		result(1, i * 3) = col1[1];
+		result(2, i * 3) = col1[2];
 
-        Vector3d col2 = (p - newP) / THETA_CHANGE;
+		p = rotateY(original, joints[i].theta[1]);
+		newP = rotateY(original, newTheta[1]);
 
-        result(i * 3 + 1, 0) = col2[0];
-        result(i * 3 + 1, 1) = col2[1];
-        result(i * 3 + 1, 2) = col2[2];
+		Vector3d col2 = (newP - p) / THETA_CHANGE;
 
-        p = rotateZ(original, joints[i].theta[2]);
-        newP = rotateZ(original, newTheta[2]);
+		result(0, i * 3 + 1) = col2[0];
+		result(1, i * 3 + 1) = col2[1];
+		result(2, i * 3 + 1) = col2[2];
 
-        Vector3d col3 = (p - newP) / THETA_CHANGE;
+		p = rotateZ(original, joints[i].theta[2]);
+		newP = rotateZ(original, newTheta[2]);
 
-        result(i * 3 + 2, 0) = col3[0];
-        result(i * 3 + 2, 1) = col3[1];
-        result(i * 3 + 2, 2) = col3[2];
-    }
+		Vector3d col3 = (newP - p) / THETA_CHANGE;
 
-    return result;
+		result(0, i * 3 + 2) = col3[0];
+		result(1, i * 3 + 2) = col3[1];
+		result(2, i * 3 + 2) = col3[2];
+	}
+
+
+	return result;
 }
 
 
 void System::updateJoints(const VectorXd& dtheta) {
 
-    Vector3d newBasePoint = joints[0].basePoint;
-    Vector3d currDTheta(0.0, 0.0, 0.0);
-    for(int i = 0; i < joints.size(); i++) {
-        currDTheta = Vector3d(dtheta(0, 3*i), dtheta(0, 3*i+1), dtheta(0, 3*i+2));
-        joints[i].update(currDTheta, newBasePoint);
-        newBasePoint = joints[i].endPoint;
-    }
+	Vector3d newBasePoint = joints[0].basePoint;
+	Vector3d currDTheta(0.0, 0.0, 0.0);
+	for (int i = 0; i < joints.size(); i++) {
+		currDTheta = Vector3d(dtheta[3 * i], dtheta[3 * i + 1], dtheta[3 * i + 2]);
+		joints[i].update(currDTheta, newBasePoint);
+		newBasePoint = joints[i].endPoint;
+	}
 }
 
 
 bool System::update(Vector3d g) {
-    Vector3d gSys = g - joints[0].basePoint;
-    // if I cant reach the goal:
-    //     g = new goal that can be reached
-    Vector3d dp = g - joints[joints.size() - 1].endPoint;
-    if (dp.norm() > 0.0001) {
-        MatrixXd J = getJacobian();
+	Vector3d gSys = g - joints[0].basePoint;
 
-        VectorXd dtheta = J.jacobiSvd().solve(dp);
+	double jointsLength = 0.0;
 
-        updateJoints(dtheta);
-        return true;
-    }
+	for (int i = 0; i < joints.size(); i++) {
+		jointsLength += joints[i].length;
+	}
 
-    return false;
+	if (gSys.norm() > jointsLength) {
+		cout << "not reachable" << endl;
+	}
+	Vector3d dp = g - joints[joints.size() - 1].endPoint;
+	if (dp.norm() > 0.0001) {
+		MatrixXd J = getJacobian();
+
+		VectorXd dtheta = J.jacobiSvd(ComputeThinU | ComputeThinV).solve(dp);
+
+		updateJoints(dtheta);
+		return true;
+	}
+
+	return false;
 }
 
 
+double colors[] = { 0.0, 0.0, 1.0,
+0.0, 1.0, 0.0,
+1.0, 0.0, 0.0 };
+
+
 void System::render() {
-    for(int i = 0; i < joints.size(); i++) {
-        joints[i].render();
-    }
+	for (int i = 0; i < joints.size(); i++) {
+		glColor3d(colors[i * 3], colors[i * 3 + 1], colors[i * 3 + 2]);
+		joints[i].render();
+		// cout << (joints[i].endPoint - joints[i].basePoint).norm() << "lol" << endl;
+		cout << joints[i].endPoint << "lol" << endl;
+	}
 }
 
 
@@ -226,97 +321,103 @@ System arm(bones);
 
 
 void myReshape(int w, int h) {
-    viewport.w = w;
-    viewport.h = h;
+	viewport.w = w;
+	viewport.h = h;
 
-    glViewport(0, 0, viewport.w, viewport.h);// sets the rectangle that will be the window
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();                // loading the identity matrix for the screen
+	glViewport(0, 0, viewport.w, viewport.h);// sets the rectangle that will be the window
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();                // loading the identity matrix for the screen
 
-    //----------- setting the projection -------------------------
-    // glOrtho sets left, right, bottom, top, zNear, zFar of the chord system
-    // glOrtho(-1, 1 + (w-400)/200.0 , -1 -(h-400)/200.0, 1, 1, -1); // resize type = add
-     glOrtho(-w/400.0, w/400.0, -h/400.0, h/400.0, 2, -2); // resize type = center
+	//----------- setting the projection -------------------------
+	// glOrtho sets left, right, bottom, top, zNear, zFar of the chord system
+	// glOrtho(-1, 1 + (w-400)/200.0 , -1 -(h-400)/200.0, 1, 1, -1); // resize type = add
+	glOrtho(-w / 400.0, w / 400.0, -h / 400.0, h / 400.0, 2, -2); // resize type = center
 
-    //glOrtho(-4, 4, -4, 4, 2, -2);    // resize type = stretch
+	//glOrtho(-4, 4, -4, 4, 2, -2);    // resize type = stretch
 
-    //------------------------------------------------------------
+	//------------------------------------------------------------
 }
 
 
+int lol;
 
+double x;
 
 void initScene(){
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Clear to black, fully transparent
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Clear to black, fully transparent
 
+	bones.push_back(Joint(Vector3d(0.0, 0.0, 0.0), 0.5));
+	bones.push_back(Joint(bones[0].endPoint, 0.2));
+	bones.push_back(Joint(bones[1].endPoint, 0.7));
 
-    bones.push_back(Joint(Vector3d(0.0, 0.0, 0.0), 1.0));
-    bones.push_back(Joint(bones[0].endPoint, 1.0));
-    bones.push_back(Joint(bones[1].endPoint, 1.0));
+	arm = System(bones);
 
-    arm = System(bones);
+	myReshape(viewport.w, viewport.h);
 
-    myReshape(viewport.w, viewport.h);
+	x = 0; 
 }
-
 
 
 
 void display() {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
+	//x = x + 1;
+	glRotated(1, 0, 1, 0);
+
+	glShadeModel(GL_FLAT);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	Vector3d goal(0.7, 0.0, 0.7);
 
 
-    glShadeModel(GL_FLAT);
+	arm.update(goal);
+	arm.render();
 
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-    arm.update(Vector3d(2.0, 2.0, 2.0));
-    arm.render();
-
-    glFlush();
-    glutSwapBuffers();
+	glFlush();
+	glutSwapBuffers();
 }
 
 //****************************************************
 // called by glut when there are no messages to handle
 //****************************************************
 void myFrameMove() {
-    //nothing here for now
+	//nothing here for now
 #ifdef _WIN32
-    Sleep(10);                                   //give ~10ms back to OS (so as not to waste the CPU)
+	Sleep(10);                                   //give ~10ms back to OS (so as not to waste the CPU)
 #endif
-    glutPostRedisplay(); // forces glut to call the display function (myDisplay())
+	glutPostRedisplay(); // forces glut to call the display function (myDisplay())
 }
 
 
 int main(int argc, char *argv[]) {
 
-    glutInit(&argc, argv);
+	glutInit(&argc, argv);
 
-    //This tells glut to use a double-buffered window with red, green, and blue channels
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	//This tells glut to use a double-buffered window with red, green, and blue channels
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
 
-    // Initalize theviewport size
-    viewport.w = 800;
-    viewport.h = 800;
+	// Initalize theviewport size
+	viewport.w = 800;
+	viewport.h = 800;
 
-    //The size and position of the window
-    glutInitWindowSize(viewport.w, viewport.h);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("CS184 Assignment 4");
+	//The size and position of the window
+	glutInitWindowSize(viewport.w, viewport.h);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("CS184 Assignment 4");
 
-    initScene();                                 // quick function to set up scene
+	initScene();                                 // quick function to set up scene
 
-    glEnable(GL_DEPTH_TEST);
+	glEnable(GL_DEPTH_TEST);
 
-    glutDisplayFunc(display);                    // function to run when its time to draw something
-    glutReshapeFunc(myReshape);                  // function to run when the window gets resized
-    glutIdleFunc(myFrameMove);                   // function to run when not handling any other task
-    glutMainLoop();                              // infinite loop that will keep drawing and resizing and whatever else
+	glutDisplayFunc(display);                    // function to run when its time to draw something
+	glutReshapeFunc(myReshape);                  // function to run when the window gets resized
+	glutIdleFunc(myFrameMove);                   // function to run when not handling any other task
+	glutMainLoop();                              // infinite loop that will keep drawing and resizing and whatever else
 
-    return 0;
+	return 0;
 }
